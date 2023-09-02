83fc7bee7df6589099f2530b0ebcdfee359a6608
IndiraRamalingam <indiraramalingam93@gmail.com>
2023-09-02 12:12:33 +0530

    images in project page

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,7 @@
 import Hospitalcrm from './assets/Hospital_CRM.png';
 import Password from './assets/Password_Reset.png';
 import Portfolio from './assets/Portfolio.png';
-import MakeupAPI from './assets/Makeup_API.png';
+import Makeup from './assets/Makeup.png';
 
 
 export const techStack = [
@@ -82,7 +82,7 @@ export const techStack = [
     },
     {
       title:"MakeUp Products API",
-      img:MakeupAPI,
+      img:Makeup,
       tech:"HTML5, CSS3",
       live:"https://zesty-centaur-78fc14.netlify.app/",
       frontend:"",
