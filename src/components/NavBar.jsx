12568a1936a4b050bc143df29d9b4dc313c24b69
IndiraRamalingam <indiraramalingam93@gmail.com>
2024-03-22 17:49:27 +0530

    Image sizing

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,31 +1,47 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
-import Button from 'react-bootstrap/Button';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+import React from 'react';
+import { useState } from 'react';
+import logo from '../assets/Indira_Logo.jpg';
+import hamburger from '../assets/hamburger.svg'
+import hamburger_close from '../assets/hamburger-close.svg'
+
 
 function NavBar() {
-  return (
-    <>
-    <Navbar collapseOnSelect expand="lg" className='navigation'>
-      <Container>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="ms-auto ">
-            <Nav.Link className='nav-links m-3' href="/">HOME</Nav.Link>
-            <Nav.Link className='nav-links m-3' href="/about">ABOUT</Nav.Link>
-            <Nav.Link className='nav-links m-3' href="/projects">PROJECTS</Nav.Link>
-            <Nav.Link className='nav-links m-3' href="/contact">CONTACT</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-    </>
-  )  
+	const [showAnimated, setShowAnimated] = useState(false);
+	return (
+		<>
+			<div className='header_img'>
+				<img />
+			</div>
+			<nav className="navbar navbar-expand-lg ">
+				<div className="container">
+					<a className="navbar-brand" href="#">
+          <img src={logo} style={{width:'50px',height:'50px', borderRadius: '50%'}}/>
+					</a>
+					<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+						<div onClick={() => setShowAnimated((prev) => !prev)}>
+							{showAnimated ? <img src={hamburger_close} /> : <img src={hamburger} />}
+						</div>
+					</button>
+					<div className="collapse navbar-collapse" id="navbarSupportedContent">
+						<ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+							<li className="nav-item">
+								<a className="nav-link" href="/">HOME</a>
+							</li>
+							<li className="nav-item">
+								<a className="nav-link" href="/about">ABOUT</a>
+							</li>
+							<li className="nav-item">
+								<a className="nav-link"  href="/projects">PROJECTS</a>
+							</li>
+							<li className="nav-item">
+								<a className="nav-link" href="/contact">CONTACT</a>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</nav>
+		</>
+	)
 }
-
-
-
 
 export default NavBar
